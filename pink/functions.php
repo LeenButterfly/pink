9782ee6ad2dfb0b097bc317d209ddb765aee5386
LeenButterfly <leenbutterfly77@yahoo.com>
2017-04-07 12:41:41 +0200

    Add files via upload

--- pink/functions.php
+++ pink/functions.php
@@ -7,5 +7,8 @@ require_once("includes/pink-filters.php");
 
 //actions
 require_once("includes/pink-actions.php");
-
+//bootstrap 
 require_once("includes/wp-bootstrap-navwalker.php");
+
+//shortcode
+require_once("includes/shortcode.php");
